136080854d24868656256c99439c53e541e3c096
Sofia Darke <sofia.darke@hyperisland.se>
2023-05-08 21:21:36 +0200

    setting up backend

---
+++ index.js
@@ -0,0 +1,31 @@
+import fetch from "node-fetch";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+const port = 8000;
+
+const url = "https://v2.jokeapi.dev/joke/Programming,Pun?safe-mode&type=";
+
+// API endpoint to fetch jokes
+app.get("/jokes", (req, res) => {
+  const { type, amount } = req.query;
+  const apiUrl = `${url}${type}&amount=${amount}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => res.json(data.jokes))
+    .catch((error) => console.error(error));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
